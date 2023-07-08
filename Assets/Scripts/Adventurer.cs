767bdeff0e4d0e558808305b0996dd1eeb0f7668
Niamh Stover <niamhastover@gmail.com>
2023-07-08 13:26:50 -0400

    Made arm movement no longer physics based

--- Assets/Scripts/Adventurer.cs
+++ Assets/Scripts/Adventurer.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 
 public class Adventurer : MonoBehaviour
 {
-    public Transform swordAnchor, bodyAnchor;
-    public Transform body, head, arm;
+    public Transform swordAnchor, rightArmAnchor, leftArmAnchor;
+    public Transform body, head, rightArm, leftArm;
+    public float minLeftArmAngle, maxLeftArmAngle;
+    public float armRotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         swordAnchor = GameObject.FindGameObjectWithTag("SwordAnchor").transform;
+        leftArm.rotation = Quaternion.Euler(0, 0, minLeftArmAngle);
     }
 
     // Update is called once per frame
     void Update()
     {
         body.right = swordAnchor.position - body.position;
-        arm.position = (bodyAnchor.position + swordAnchor.position) / 2;
-        arm.up = swordAnchor.position - bodyAnchor.position;
+        rightArm.position = (rightArmAnchor.position + swordAnchor.position) / 2;
+        rightArm.up = swordAnchor.position - rightArmAnchor.position;
+        leftArm.position = leftArmAnchor.position;
+        if (Input.GetMouseButton(0))
+        {
+            leftArm.rotation = Quaternion.RotateTowards(leftArm.rotation, Quaternion.Euler(0, 0, body.rotation.eulerAngles.z + maxLeftArmAngle), armRotationSpeed);
+        }
+        else
+        {
+            leftArm.rotation = Quaternion.RotateTowards(leftArm.rotation, Quaternion.Euler(0, 0, body.rotation.eulerAngles.z + minLeftArmAngle), armRotationSpeed);
+        }
         head.up = -(swordAnchor.position - body.position);
         /*
         Vector2 oldArmAnchor1 = armAnchor1.position;

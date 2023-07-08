74bb815173dd04f9fb6f1e7fbe9f5990f757532d
Aria Johnson <rhythmicaria@gmail.com>
2023-07-07 23:23:34 -0500

    Rotating Mage Sprite

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Archer : MonoBehaviour
+public class Missile : MonoBehaviour
 {
-    public GameObject Target;
+    public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class Archer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.right * speed * Time.deltaTime;
     }
 }

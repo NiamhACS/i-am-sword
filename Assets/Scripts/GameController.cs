36a6574f313fe47c269d29f9d48a1eb4c073ac66
Niamh Stover <niamhastover@gmail.com>
2023-07-09 08:36:21 -0400

    Corpses Added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
+    public Transform corpseParent;
+    public GameObject corpsePrefab;
+    public Queue<GameObject> corpses;
     public int score = 0;
     public float[] timeThresholds;
     public int currentEnemies = 0;
@@ -15,6 +18,11 @@ public class GameController : MonoBehaviour
     void Start()
     {
         instance = this;
+        corpses = new();
+        for (int i = 0; i < 50; i++)
+        {
+            corpses.Enqueue(Instantiate(corpsePrefab, corpseParent));
+        }
     }
 
     // Update is called once per frame
@@ -32,5 +40,19 @@ public class GameController : MonoBehaviour
             SpawnData enemy = EnemySpawner.instance.enemies[Random.Range(0, currentEnemies + 1)];
             nextInterval += EnemySpawner.instance.Spawn(enemy);
         }
+    }
+
+    public void SpawnCorpse(Enemy enemy)
+    {
+        Corpse newCorpse = corpses.Dequeue().GetComponent<Corpse>();
+        newCorpse.sprites = enemy.corpseSprites;
+        newCorpse.transform.position = enemy.transform.position;
+        newCorpse.lifeSpan = 4;
+        newCorpse.gameObject.SetActive(true);
+    }
+
+    public void GainScore(int score)
+    {
+        this.score += score;
     }
 }

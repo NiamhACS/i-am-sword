14dfeab668b4d7c6f029ff5f81e34c07612186d9
Aria Johnson <rhythmicaria@gmail.com>
2023-07-09 02:10:00 -0500

    Background Elements Added, Human Updated

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int score = 0;
+
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        text.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        text.text = score.ToString();
+    }
+}
